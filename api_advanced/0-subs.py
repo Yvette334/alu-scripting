c76eaa83bb78c23a350c8428d4d8959382541251
Yvette334 <y.muhoracye@alustudent.com>
2025-02-17 17:25:30 +0000

    added to repository

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -10,16 +10,15 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Python/requests"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 404:
         return 0
-    
+
     try:
         results = response.json().get("data")
         if results:
             return results.get("subscribers", 0)
     except ValueError:
         return 0
-    
-    return 0
 
+    return 0
