551faf2fe9355a55640d22ef2dfe7a35a7fc800a
Yvette334 <y.muhoracye@alustudent.com>
2025-02-17 18:11:48 +0000

    added to repository

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -15,35 +15,25 @@ def top_ten(subreddit):
     '''
     api_headers = {
         'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
     }
-    sort = 'top'
-    limit = 10
-
-    # Make the GET request to Reddit's API
-    res = requests.get(
-        '{}/r/{}/.json?sort={}&limit={}'.format(
-            BASE_URL,
-            subreddit,
-            sort,
-            limit
-        ),
-        headers=api_headers,
-        allow_redirects=False  # Prevent following redirects
-    )
     
-    # Check for a successful response (status code 200)
+    # Construct the URL to access the subreddit
+    url = f'{BASE_URL}/r/{subreddit}/.json?sort=top&limit=10'
+    
+    # Send the GET request
+    res = requests.get(url, headers=api_headers, allow_redirects=False)
+    
+    # Check if we have a valid response (status code 200)
     if res.status_code == 200:
         try:
-            # Retrieve and print the top 10 posts from the response
-            for post in res.json()['data']['children'][0:10]:
-                print(post['data']['title'])
+            # Parse the JSON response and retrieve the top 10 posts
+            posts = res.json().get('data', {}).get('children', [])
+            if posts:
+                for post in posts:
+                    print(post['data']['title'])
+            else:
+                print(None)
         except (KeyError, ValueError):
             print(None)
     else:
