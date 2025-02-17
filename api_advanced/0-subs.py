a9390b82f2a260873d4da9a4fadad5c97cadcba7
Yvette334 <y.muhoracye@alustudent.com>
2025-02-17 17:12:17 +0000

    added to repository

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python3
-'''A module containing functions for working with the Reddit API.
-'''
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Python/requests"}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
