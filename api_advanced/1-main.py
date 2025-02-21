cf8c27a3413a11ed0302110d8e32b645a00a2e84
Yvette334 <y.muhoracye@alustudent.com>
2025-02-21 15:26:42 +0000

    added

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """
-Main module for testing the `top_ten` function.
-
-This script takes a subreddit name as a command line argument,
-and calls the `top_ten` function to display the top 10 hot posts
-for the given subreddit. If no argument is passed, it will prompt
-the user to provide a subreddit name.
+1-main
 """
 import sys
 
@@ -15,4 +10,3 @@ if __name__ == '__main__':
         print("Please pass an argument for the subreddit to search.")
     else:
         top_ten(sys.argv[1])
-
