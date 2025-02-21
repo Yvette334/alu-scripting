47dc7b5941d417c0ffaf8c999abf057bbe8f9a65
Yvette334 <y.muhoracye@alustudent.com>
2025-02-21 20:39:00 +0000

    added

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,33 +1,52 @@
 #!/usr/bin/python3
+
 """
+
 Module that queries the Reddit API and prints the titles
+
 of the first 10 hot posts listed for a given subreddit.
+
 """
+
 import requests
-import sys
-
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the
-    first 10 hot posts for a given subreddit.
 
-    Args:
-        subreddit: The subreddit to query
-    """
-    headers = {'User-Agent': 'python:reddit.api.client:v1.0'}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+"""
 
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code != 200:
-            sys.stdout.write("OK")
-            return
-        data = response.json().get('data', {}).get('children', [])
-        if not data:
-            sys.stdout.write("OK")
-            return
-        for post in data:
-            print(post.get('data', {}).get('title'))
-    except Exception:
-        sys.stdout.write("OK")
+Queries the Reddit API and prints the titles of the
+
+first 10 hot posts for a given subreddit.
+Args:
+
+subreddit: The subreddit to query
+
+"""
+
+headers = {'User-Agent': 'python:reddit.api.client:v1.0'}
+
+url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+response = requests.get(url, headers=headers, allow_redirects=False)
+
+if response.status_code != 200:
+
+print("OK",end='')
+
+return
+
+try:
+
+data = response.json().get('data', {}).get('children', [])
+
+if not data:
+
+print("OK",end='')
+
+return
+
+for post in data:
+
+print(post.get('data', {}).get('title'))
+
+except Exception:
+
+print("OK",end='')
