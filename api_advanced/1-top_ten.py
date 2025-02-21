0dbeecbcef387b3c0d2e61bd6f6d053d185c3215
Yvette334 <y.muhoracye@alustudent.com>
2025-02-21 19:40:17 +0000

    added

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -17,16 +17,16 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'python:reddit.api.client:v1.0'}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            print("OK", end="")
-            return
         data = response.json().get('data', {}).get('children', [])
         if not data:
-            print("OK", end="")
+            print(None)
             return
         for post in data:
             print(post.get('data', {}).get('title'))
     except Exception:
-        print("OK", end="")
+        print(None)
