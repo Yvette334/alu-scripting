55db9b0e4092104357cf332c5e69c2152b41b534
Yvette334 <y.muhoracye@alustudent.com>
2025-02-21 20:43:48 +0000

    added

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 
 """
-
 Module that queries the Reddit API and prints the titles
 
 of the first 10 hot posts listed for a given subreddit.
-
 """
 
 import requests
 def top_ten(subreddit):
 
 """
-
 Queries the Reddit API and prints the titles of the
 
 first 10 hot posts for a given subreddit.
 Args:
-
 subreddit: The subreddit to query
-
 """
 
 headers = {'User-Agent': 'python:reddit.api.client:v1.0'}
@@ -29,7 +24,7 @@ response = requests.get(url, headers=headers, allow_redirects=False)
 
 if response.status_code != 200:
 
-print("OK",end='')
+print("OK", end='')
 
 return
 
@@ -39,7 +34,7 @@ data = response.json().get('data', {}).get('children', [])
 
 if not data:
 
-print("OK",end='')
+print("OK", end='')
 
 return
 
@@ -49,4 +44,4 @@ print(post.get('data', {}).get('title'))
 
 except Exception:
 
-print("OK",end='')
+print("OK", end='')
