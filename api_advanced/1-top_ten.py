d4243a04568b72252990804240993cc98800e400
Yvette334 <y.muhoracye@alustudent.com>
2025-02-21 20:20:00 +0000

    added

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -19,14 +19,14 @@ def top_ten(subreddit):
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        print("OK", end='')
+        print("OK", end='')  # Change: Remove extra space
         return
     try:
         data = response.json().get('data', {}).get('children', [])
         if not data:
-            print("OK", end='')
+            print("OK", end='')  # Change: Remove extra space
             return
         for post in data:
             print(post.get('data', {}).get('title'))
     except Exception:
-        print("OK", end='')
+        print("OK", end='')  # Change: Remove extra space
